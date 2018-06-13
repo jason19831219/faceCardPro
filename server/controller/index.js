ca6377e28f8a7270f32d3b7b417207c21047d3d5
jasonshen <13916109998@qq.com>
2018-06-13 15:50:00 +0800

    star

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,6 +1,3 @@
 exports.Admin = require('./admin');
 exports.User = require('./user');
 exports.Star = require('./star');
-exports.Article = require('./article');
-exports.Moji = require('./moji');
-exports.MojiSet = require('./mojiSet');
