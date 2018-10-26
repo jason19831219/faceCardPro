26430551f6682577ec6e21368d8f6b56c2bac280
jasonshen <13916109998@qq.com>
2018-10-26 21:35:45 +0800

    ignore

--- www-admin/src/view/FaceCard.vue
+++ www-admin/src/view/FaceCard.vue
@@ -49,109 +49,109 @@
       <el-table-column
         prop="likeNum"
         label="likeNum"
-        width="400"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="likeUserIds"
         label="likeUserIds"
-        width="400"
+        width="100"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="clickNum"
         label="clickNum"
-        width="400"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="isTop"
         label="isTop"
-        width="400"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="street"
         label="street"
-        width="400"
+        width="100"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="district"
         label="district"
-        width="400"
+        width="100"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="city"
         label="city"
-        width="400"
+        width="100"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="age"
         label="age"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="hairParam"
         label="hairParam"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="hairQuality"
         label="hairQuality"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="hairQuantity"
         label="hairQuantity"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="hairGranularity"
         label="hairGranularity"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="hairCrispation"
         label="hairCrispation"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="yaw"
         label="yaw"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="pitch"
         label="pitch"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="roll"
         label="roll"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="beauty"
         label="beauty"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="expression"
         label="expression"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
@@ -163,25 +163,25 @@
       <el-table-column
         prop="face_shape"
         label="face_shape"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="face_token"
         label="face_token"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="gender"
         label="gender"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
         prop="glasses"
         label="glasses"
-        width="200"
+        width="50"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
